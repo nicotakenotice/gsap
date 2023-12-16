8341320b92805b157751657e9c1f4cd99dbf2e1f
Nico <nicola.zorzo@gmail.com>
2023-12-16 17:56:17 +0100

    update layout, start timeline as paused

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,21 +1,25 @@
 import { AfterViewInit, Component } from '@angular/core';
+import { TimelineControllerComponent } from '@lib/components';
 import gsap from 'gsap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TimelineControllerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements AfterViewInit {
-  tl = gsap.timeline();
+  tl = gsap.timeline({ paused: true });
 
   constructor() { }
 
   ngAfterViewInit(): void {
+    this.buildTimeline();
+  }
+
+  buildTimeline(): void {
     this.tl
-      .to('.letter', { color: 'green', duration: 1, stagger: 1 })
-      .pause();
+      .to('.letter', { color: 'green', duration: 1, stagger: 1 });
   }
 }
